35b744cd79fd5838695f973d0d84bd41afcf14d4
Andy <yoongi9289@gmail.com>
2023-09-26 10:48:59 -0600

    add: server zip for aws

--- server/app.js
+++ server/app.js
@@ -1,14 +1,8 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
-import express, { response } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import randomRoute from './routes/randomRoute.js';
 import searchRoute from './routes/searchRoute.js';
-import { fileURLToPath } from 'url';
-import axios from 'axios';
-
-const __filename = fileURLToPath(import.meta.url);
 
 const app = express();
 
@@ -21,33 +15,12 @@ app.use(bodyParser.json());
 
 let port = process.env.port || 3001;
 
-app.get('/', (req, res)=> {
-  res.send('Server side running!')
-});
-
 app.use('/randomfood', randomRoute);
 app.use('/searchfoods', searchRoute);
 
-// // console.log(currentDay)
-// let state = "co"
-// let city = 'aurora';
-// // https://www.eventbrite.com/d/nv--las-vegas/events--today/?page=1
-
-// // app.use('/store-data', weatherRoutes);
-// // use axios call below for webscraping data for trips
-// let url = `https://www.eventbrite.com/d/${state}--${city}/free--events--today/?page=1`;
-// let data = [];
-// axios.get(url)
-//   .then((response) => {
-//     const $ = cheerio.load(response.data);
-//     // const events = $('.discover-search-desktop-card h2').toArray().slice(2,5);
-//     const timesAndLocals = $('.discover-horizontal-event-card .event-card__clamp-line--one').toArray().slice(4,10); // have to times the num's by 2 
-//     // events.forEach(e=>console.log(e.children[0].data))
-//     timesAndLocals.forEach(e=>console.log(e.children[0].data))
-//   })
-//   .catch((error)=>console.error('Error: ', error)
-// );
-
+app.get('/', (req, res)=> {
+  res.send('Server side running!')
+});
 
 app.listen(port, function (){
   console.log(`Server is running on port ${port}`);
