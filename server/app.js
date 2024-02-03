94cde110fc59d5f78426cf44c0c9783ec8a51800
Andy <yoongi9289@gmail.com>
2024-02-02 21:23:16 -0700

    Remove unnecessary comments

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import randomRoute from './routes/randomRoute.js';
-import searchRoute from './routes/searchRoute.js';
-import ServerlessHttp from 'serverless-http';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import randomRoute from "./routes/randomRoute.js";
+import searchRoute from "./routes/searchRoute.js";
+import ServerlessHttp from "serverless-http";
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(cors());
 
@@ -16,15 +16,16 @@ app.use(bodyParser.json());
 
 let port = process.env.port || 3001;
 
-app.use('/randomfood', randomRoute);
-app.use('/searchfoods', searchRoute);
+app.use("/randomfood", randomRoute);
+app.use("/searchfoods", searchRoute);
 
-module.exports.handler = ServerlessHttp(app);
+// might need this for aws commented out on local
+// module.exports.handler = ServerlessHttp(app);
 
-app.get('/', (req, res)=> {
-  res.send('Server side running!')
+app.get("/", (req, res) => {
+  res.send("Server side running!");
 });
 
-app.listen(port, function (){
+app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
 });
