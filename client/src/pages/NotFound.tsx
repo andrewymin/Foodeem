e47fa435d58ae5a45bd550613a8da06d6538b794
Andy <yoongi9289@gmail.com>
2024-05-30 12:22:06 -0600

    add 404 error page

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+function NotFound() {
+  return (
+    <section id="error-page">
+      <h1>
+        <span>404</span>
+        <br />
+        Got Lost In The Sauce
+      </h1>
+    </section>
+  );
+}
+
+export default NotFound;
