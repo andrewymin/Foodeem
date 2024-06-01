57d6ef8181b65ccd724e742ad6366bbb25e8709b
Andy <yoongi9289@gmail.com>
2024-06-01 10:50:25 -0600

    create sign up form

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,13 +1,12 @@
-import BgImg from "../assets/img/about_bg.avif";
+import { useLayoutEffect } from "react";
 
 function About() {
-  // const aboutImg =
-  //   "https://images.unsplash.com/photo-1495195134817-aeb325a55b65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1176&q=80";
-  const aboutImg = BgImg;
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <section id="about">
-      {/* <img src={aboutImg} alt="Cooking Board" /> */}
       <div className="content">
         <h1>About Us</h1>
         <h2 className="title">Finding Great Recipes</h2>
